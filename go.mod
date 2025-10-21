56358469e50c43ce05bb0b1f1b5d50fad4192c29
Midia Zamani <zamani.midia@gmail.com>
2025-10-21 22:10:47 +0200

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MidiaNova/puppy
 
 go 1.25.1
+
+require github.com/MidiaNova/dog v0.0.0-20251021192715-9587d3d506f0 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/MidiaNova/dog"
+)
+
 func Bark() string {
 	return "hup!"
 }
 
 func Barks() string {
 	return "hup! hup! hup!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
